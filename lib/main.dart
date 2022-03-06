2ace4993a518365377345a767695e83bad87ca54
spring98 <foreverspring98@gmail.com>
2022-03-06 16:04:18 +0900

    original wiring-unit

---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+// ignore_for_file: avoid_print
+
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:usb_serial/transaction.dart';
+import 'package:usb_serial/usb_serial.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  UsbPort? _port;
+  String _status = "Idle";
+  List<Widget> _ports = [];
+  final List<Widget> _serialData = [];
+
+  UsbDevice? _device;
+
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    UsbSerial.usbEventStream!.listen((UsbEvent event) {
+      _getPorts();
+    });
+
+    _getPorts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectTo(null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('USB Serial Plugin example app'),
+      ),
+      body: Center(
+          child: Column(children: <Widget>[
+        Text(
+            _ports.isNotEmpty
+                ? "Available Serial Ports"
+                : "No serial devices available",
+            style: Theme.of(context).textTheme.headline6),
+        ..._ports,
+        Text('Status: $_status\n'),
+        Text('info: ${_port.toString()}\n'),
+        ListTile(
+          title: TextField(
+            controller: _textController,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Text To Send',
+            ),
+          ),
+          trailing: ElevatedButton(
+            child: const Text("Send"),
+            onPressed: _port == null
+                ? null
+                : () async {
+                    if (_port == null) {
+                      return;
+                    }
+                    String data = _textController.text + "\r\n";
+                    await _port!.write(Uint8List.fromList(data.codeUnits));
+                    _textController.text = "";
+                  },
+          ),
+        ),
+        Text("Result Data", style: Theme.of(context).textTheme.headline6),
+        ..._serialData,
+      ])),
+    ));
+  }
+
+  Future<bool> _connectTo(device) async {
+    _serialData.clear();
+
+    if (_port != null) {
+      _port!.close();
+      _port = null;
+    }
+
+    if (device == null) {
+      _device = null;
+      setState(() {
+        _status = "Disconnected";
+      });
+      return true;
+    }
+
+    _port = await device.create();
+    if (await (_port!.open()) != true) {
+      setState(() {
+        _status = "Failed to open port";
+      });
+      return false;
+    }
+    _device = device;
+
+    await _port!.setDTR(true);
+    await _port!.setRTS(true);
+    await _port!.setPortParameters(
+        9600, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+    _port!.inputStream?.listen((event) {
+      print(event);
+    });
+
+    setState(() {
+      _status = "Connected";
+    });
+    return true;
+  }
+
+  void _getPorts() async {
+    _ports = [];
+    List<UsbDevice> devices = await UsbSerial.listDevices();
+    if (!devices.contains(_device)) {
+      _connectTo(null);
+    }
+    print(devices);
+
+    for (var device in devices) {
+      _ports.add(ListTile(
+          leading: const Icon(Icons.usb),
+          title: Text(device.productName!),
+          subtitle: Text(device.manufacturerName ?? '이름을 설정해주세요'),
+          trailing: ElevatedButton(
+            child: Text(_device == device ? "Disconnect" : "Connect"),
+            onPressed: () {
+              _connectTo(_device == device ? null : device).then((res) {
+                _getPorts();
+              });
+            },
+          )));
+    }
+
+    setState(() {
+      print(_ports);
+    });
+  }
+}
